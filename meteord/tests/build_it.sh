04960ffb5e033edbd7035e7d09fa13b449a5c9f1
Dmitry Romanov <me@wutiarn.ru>
2016-12-29 10:58:57 +0300

    remove old images

--- meteord/tests/build_it.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker build -t meteorhacks/meteord:base ../base
-docker build -t meteorhacks/meteord:onbuild ../onbuild
-docker build -t meteorhacks/meteord:devbuild ../devbuild
-docker build -t meteorhacks/meteord:binbuild ../binbuild
