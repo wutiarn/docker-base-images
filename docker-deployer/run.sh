88df2b15b7fd447a37b1ded3f60ea40aed6e659a
Dmitry Romanov <me@wutiarn.ru>
2017-03-30 15:57:58 +0300

    Update run.sh

--- docker-deployer/run.sh
+++ docker-deployer/run.sh
@@ -3,6 +3,6 @@
 
 curl -sSL ${CA_URL} > /root/.docker/ca.pem
 curl -sSL ${CERT_URL} > /root/.docker/cert.pem
-echo ${KEY} | base64 -d > /root/.docker/key.pem
+echo ${DOCKER_KEY} | base64 -d > /root/.docker/key.pem
 
 $@
